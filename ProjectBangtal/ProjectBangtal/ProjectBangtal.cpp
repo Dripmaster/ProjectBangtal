28b27583c8acd245551bb993e673c9dfea60f28f
Dripmaster <son342@gmail.com>
2020-07-19 02:41:14 +0900

    first commit

---
+++ ProjectBangtal/ProjectBangtal/ProjectBangtal.cpp
@@ -0,0 +1,10 @@
+﻿// ProjectBangtal.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <Bangtal.h>
+SceneID firstScene;
+int main()
+{
+	firstScene = createScene("방1","Images/배경-1.png");
+	startGame(firstScene);
+}
